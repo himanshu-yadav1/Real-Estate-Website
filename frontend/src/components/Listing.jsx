eb568f67c421af3499d97f0198e19e716f531e76
Himanshu <himanshuydv7777@gmail.com>
2024-06-19 01:20:10 +0530

    Improve UI for Listing component

--- frontend/src/components/Listing.jsx
+++ frontend/src/components/Listing.jsx
@@ -8,18 +8,17 @@ import {
     FaBath,
     FaBed,
     FaChair,
-    FaMapMarkedAlt,
     FaMapMarkerAlt,
     FaParking,
     FaShare,
-  } from 'react-icons/fa';
+} from 'react-icons/fa'
 
 function Listing() {
     SwiperCore.use([Navigation])
     const params = useParams()
     const [listingData, setListingData] = useState(null)
     const [error, setError] = useState(false)
-    const [copied, setCopied] = useState(false);
+    const [copied, setCopied] = useState(false)
 
     useEffect(() => {
         const fetchListing = async () => {
@@ -31,125 +30,130 @@ function Listing() {
                 method: "GET",
                 credentials: 'include' // Ensures cookies are sent with the request
             })
-            .then((resp) => {
-                return resp.json()
-            })
-            .then((data) => {
-                if(data.success === false){
-                    setError(true)
-                    return
-                }
+                .then((resp) => {
+                    return resp.json()
+                })
+                .then((data) => {
+                    if (data.success === false) {
+                        setError(true)
+                        return
+                    }
 
-                setListingData(data)
-                setError(false)
-            })
-            .catch((error) => {
-                console.log(error)
-                setError(true)
-            })
+                    setListingData(data)
+                    setError(false)
+                })
+                .catch((error) => {
+                    console.log(error)
+                    setError(true)
+                })
         }
 
         fetchListing()
     }, [])
 
-  return (
-    <main>
-        {
-            error &&
-            <p className="text-xl text-center mt-5 mx-5">Something went wrong while fetching listing</p>
-        }
+    return (
+        <main className="p-5">
+            {
+                error &&
+                <p className="text-xl text-center mt-5 mx-5">Something went wrong while fetching listing</p>
+            }
 
-        {
-            listingData && !error && (
-                <div>
-                    <Swiper navigation>
-                        {listingData.imageUrls.map((url) => (
-                            <SwiperSlide key={url}>
-                                <div className='h-[60vh] mt-1 w-[100vw] md:w-[60vw] mx-auto rounded-xl' style={{ 
-                                        background: `url(${url}) center no-repeat`, 
+            {
+                listingData && !error && (
+                    <div>
+                        <Swiper navigation>
+                            {listingData.imageUrls.map((url) => (
+                                <SwiperSlide key={url}>
+                                    <div className='h-[60vh] mt-1 w-[100vw] md:w-[60vw] mx-auto rounded-xl' style={{
+                                        background: `url(${url}) center no-repeat`,
                                         backgroundSize: 'cover',
                                     }}
-                                >
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                                    >
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
 
-                    <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
-                        <FaShare
-                        className='text-slate-500'
-                            onClick={() => {
-                                navigator.clipboard.writeText(window.location.href);
-                                setCopied(true);
-                                
-                                setTimeout(() => {
-                                    setCopied(false);
-                                }, 2000);
-                            }}
-                        />
-                    </div>
+                        <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+                            <FaShare
+                                className='text-slate-500'
+                                onClick={() => {
+                                    navigator.clipboard.writeText(window.location.href);
+                                    setCopied(true);
 
-                    {copied && (
-                        <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
-                            Link copied!
-                        </p>
-                    )}
-
-                    <div className='flex flex-col w-[100vw] md:w-[60vw] mx-auto p-3 my-7 gap-4'>
-                        <p className='text-2xl font-semibold'>
-                            {listingData.title} - ₹
-                            {listingData.offer ? listingData.discountedPrice : listingData.regularPrice}
-                            {listingData.type === 'rent' ? ' / month' : ''}
-                        </p>
-
-                        <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
-                            <FaMapMarkerAlt className='text-green-700' />
-                            {listingData.address}
-                        </p>
-                        
-                        <div className='flex gap-4'>
-                            <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                                {listingData.type === 'rent' ? 'For Rent' : 'For Sale'}
-                            </p>
-
-                            {listingData.offer && (
-                                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                                    <span>₹ {+listingData.regularPrice - +listingData.discountedPrice}</span>
-                                    <span> Discount</span>
-                                </p>
-                            )}
+                                    setTimeout(() => {
+                                        setCopied(false);
+                                    }, 2000);
+                                }}
+                            />
                         </div>
 
-                        <p className='text-slate-800'>
-                            <span className='font-semibold text-black'>Description - </span>
-                            {listingData.description}
-                        </p>
+                        {copied && (
+                            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+                                Link copied!
+                            </p>
+                        )}
 
-                        <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
-                            <li className='flex items-center gap-1 whitespace-nowrap '>
-                                <FaBed className='text-lg' />
-                                {listingData.bedrooms > 1 ? `${listingData.bedrooms} beds ` : `${listingData.bedrooms} bed `}
-                            </li>
-                            <li className='flex items-center gap-1 whitespace-nowrap '>
-                                <FaBath className='text-lg' />
-                                {listingData.bathrooms > 1 ? `${listingData.bathrooms} baths ` : `${listingData.bathrooms} bath `}
-                            </li>
-                            <li className='flex items-center gap-1 whitespace-nowrap '>
-                                <FaParking className='text-lg' />
-                                {listingData.parking ? 'Parking spot' : 'No Parking'}
-                            </li>
-                            <li className='flex items-center gap-1 whitespace-nowrap '>
-                                <FaChair className='text-lg' />
-                                {listingData.furnished ? 'Furnished' : 'Unfurnished'}
-                            </li>
-                        </ul>
+                        <div className='flex flex-col w-[90vw] md:w-[60vw] mx-auto p-3 my-7 gap-4'>
+                            <p className='text-2xl font-semibold'>
+                                {listingData.title} - ₹
+                                {listingData.offer ? listingData.discountedPrice : listingData.regularPrice}
+                                {listingData.type === 'rent' ? ' / month' : ''}
+                            </p>
+
+                            <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+                                <FaMapMarkerAlt className='text-green-700' />
+                                {listingData.address}
+                            </p>
+
+                            <div className='flex gap-4'>
+                                <span className='bg-red-100 text-red-900 px-3 py-1 rounded-xl border border-red-200'>
+                                    {listingData.type === 'rent' ? 'For Rent' : 'For Sale'}
+                                </span>
+
+                                {listingData.offer && (
+                                    <span className='bg-green-200 text-green-900 px-3 py-1 rounded-xl border border-green-200'>
+                                        ₹{+listingData.regularPrice - +listingData.discountedPrice} Discount
+                                    </span>
+                                )}
+                            </div>
+
+                            <p className='text-slate-800'>
+                                <span className='font-semibold text-black'>Description - </span>
+                                {listingData.description}
+                            </p>
+
+                            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaBed className='text-lg' />
+                                    {listingData.bedrooms > 1 ? `${listingData.bedrooms} beds ` : `${listingData.bedrooms} bed `}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaBath className='text-lg' />
+                                    {listingData.bathrooms > 1 ? `${listingData.bathrooms} baths ` : `${listingData.bathrooms} bath `}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaParking className='text-lg' />
+                                    {listingData.parking ? 'Parking spot' : 'No Parking'}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaChair className='text-lg' />
+                                    {listingData.furnished ? 'Furnished' : 'Unfurnished'}
+                                </li>
+                            </ul>
+
+                            <div className="bg-blck flex justify-center">
+                                <button className='cursor-not-allowed hover:opacity-70 w-[100%] sm:w-[50%] mt-5 bg-green-700 text-white px-4 py-2 rounded-lg'>
+                                    Contact Owner
+                                </button>
+                            </div>
+                        </div>
+
                     </div>
-
-                </div>
-            )
-        }
-    </main>
-  )
+                )
+            }
+        </main>
+    )
 }
 
 export default Listing
